e4d5f526df44d7f6cec063c36e41a6394922bab8
github_akeenola <akinola.olabisi@gmail.com>
2023-09-12 12:11:45 -0700

    edit pix size

--- action.js
+++ action.js
@@ -8,3 +8,10 @@ let clock = "The hour is " + time%12;
 //let ampm = (time> 11)?"PM":"AM";
 console.log(`half of 100 is ${utcTime}`);
 console.log(clock  );
+function getDay(){
+    const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    const time = days[new Date().getDay()];
+    document.getElementById("CurrentDayOfTheWeek").innerHTML = "time is"+time;
+    console.log("New function given day"+time);
+
+}
