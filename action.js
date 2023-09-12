18ddc3943ab8d5e15ecf2d55b98751c7409bca9b
github_akeenola <akinola.olabisi@gmail.com>
2023-09-12 11:09:08 -0700

    web app files

---
+++ action.js
@@ -0,0 +1,10 @@
+const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const time = days[new Date().getDay()];
+const utcTime = new Date().getUTCMilliseconds();
+//window.onload(alert("I am alive \n"+time));
+console.log(time);
+console.log("Your name"+time%12);
+let clock = "The hour is " + time%12;
+//let ampm = (time> 11)?"PM":"AM";
+console.log(`half of 100 is ${utcTime}`);
+console.log(clock  );
